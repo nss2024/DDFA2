21f191e6b320aeb344d648a6d9e1cfe53fe36da7
nss2024 <hannuri2022@gmail.com>
2024-10-28 17:37:32 +0900

    Update run2.sh

--- run2.sh
+++ run2.sh
@@ -12,12 +12,19 @@ rm -rf /content/DeepDFA/DDFA/storage/cache
 rm -rf /content/DeepDFA/DDFA/storage/processed/bigvul
 mkdir /content/DeepDFA/DDFA/storage/processed/bigvul
 cd /content/DeepDFA/DDFA/sastvd/scripts
-python /content/DeepDFA/DDFA/sastvd/scripts/prepare.py --dataset bigvul
-python /content/DeepDFA/DDFA/sastvd/scripts/getgraphs.py bigvul --overwrite
-python /content/DeepDFA/DDFA/sastvd/scripts/dbize.py
-python /content/DeepDFA/DDFA/sastvd/scripts/dbize_graphs.py
-python /content/DeepDFA/DDFA/sastvd/scripts/abstract_dataflow_full.py --no-cache --stage 1
-python /content/DeepDFA/DDFA/sastvd/scripts/abstract_dataflow_full.py --no-cache --stage 2
-python /content/DeepDFA/DDFA/sastvd/scripts/dbize_absdf.py
+bash /content/DeepDFA/DDFA/scripts/preprocess.sh
+
+# python /content/DeepDFA/DDFA/sastvd/scripts/prepare.py --dataset bigvul
+# python /content/DeepDFA/DDFA/sastvd/scripts/getgraphs.py bigvul --overwrite
+# python /content/DeepDFA/DDFA/sastvd/scripts/dbize.py
+# python /content/DeepDFA/DDFA/sastvd/scripts/dbize_graphs.py
+# python /content/DeepDFA/DDFA/sastvd/scripts/abstract_dataflow_full.py --no-cache --stage 1
+# python /content/DeepDFA/DDFA/sastvd/scripts/abstract_dataflow_full.py --no-cache --stage 2
+# python /content/DeepDFA/DDFA/sastvd/scripts/dbize_absdf.py
+
+
 
 python /content/DeepDFA/DDFA/code_gnn/main_cli.py fit --config /content/DeepDFA/DDFA/configs/config_bigvul.yaml --config /content/DeepDFA/DDFA/configs/config_ggnn.yaml
+
+#train
+#python code_gnn/main_cli.py fit --config configs/config_bigvul.yaml --config configs/config_ggnn.yaml $@
